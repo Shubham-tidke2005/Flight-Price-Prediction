19851b320133dbac4cb2eb5e39b9d05093d04126
Shubham Tidke <tidkeshubham826@gmail.com>
2026-06-18 23:23:54 +0530

    day-2

--- Dataset/ingest.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine                
-import pandas as pd       
-
-engine=create_engine('sqlite:///flights.db')
-df=pd.read_csv('clean_dataset.csv')
-
-df.to_sql('flights',con=engine,if_exists='replace',index=False)
-
-    
